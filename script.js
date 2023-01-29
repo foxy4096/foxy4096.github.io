11900f5cf6fc48953e62297eb0d112bbc5fb8983
Aditya Priyadarshi <adityapriyadarshi669@gmail.com>
2023-01-29 16:45:37 +0530

    Added the fetch

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-// Dark mode
-
-addEventListener("load", function () {
-  var body = document.querySelector("body");
-  var darkMode = localStorage.getItem("darkMode");
-  if (darkMode === "enabled") {
-    body.classList.add("dark");
-  }
-});
-
-function toggleDarkMode() {
-  var body = document.querySelector("body");
-  body.classList.toggle("dark");
-  if (body.classList.contains("dark")) {
-    localStorage.setItem("darkMode", "enabled");
-  } else {
-    localStorage.setItem("darkMode", "disabled");
-  }
+async function get_repos(username) {
+  const response = await fetch(
+    `https://api.github.com/users/${username}/repos`
+  );
+  const repos = await response.json();
+  console.log(repos);
+  document.getElementById("repos").innerHTML = repos
+    .map((repo) =>
+      `<div class="box">
+<h4><a href="${repo.html_url}">${repo.name}</a></h4>
+<p>${repo.description?? ""}</p>
+</div>
+`.slice(0, -1)
+    )
+    .join("");
 }
+
+get_repos("foxy4096");
