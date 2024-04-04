5d6e48ec534fae98c5f5e25053cc4770ca4f4d92
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2024-04-04 21:19:10 +0530

    Reverted back to static html :(

--- app/providers.js
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme";
-
-export function Providers({ children }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
-}
--- mdx-components.js
+++
@@ -1,9 +0,0 @@
-import { Prose } from "@nikolovlazar/chakra-ui-prose";
-
-export function useMDXComponents(components) {
-  return {
-    prose: Prose,
-    
-    ...components,
-  };
-}
